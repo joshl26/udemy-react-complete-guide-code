b9c3d24081136ea39203c8c6a0e4f2c506b1a86c
joshl26 <joshl26@hotmail.com>
2022-11-13 15:23:11 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import { useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
 
     const data = await response.json();
@@ -21,6 +23,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   const dummyMovies = [
@@ -44,7 +47,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies....</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
