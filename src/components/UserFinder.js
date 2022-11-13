4fc1e6e88c154310673cd3c170e6b3e1a462614e
joshl26 <joshl26@hotmail.com>
2022-11-13 13:41:27 -0500

    commit

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from "react";
+import { Component, Fragment } from "react";
 
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
@@ -9,30 +9,57 @@ const DUMMY_USERS = [
   { id: "u3", name: "Julie" },
 ];
 
-const UserFinder = () => {
-  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-  const [searchTerm, setSearchTerm] = useState("");
+class UserFinder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: "",
+    };
+  }
 
-  useEffect(() => {
-    setFilteredUsers(
-      DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+  componentDidMount() {
+    // Send http request....
+    this.setState({ filteredUsers: DUMMY_USERS });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
     );
-  }, [searchTerm]);
+  }
+}
 
-  const searchChangeHandler = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  return (
-    <div>
-      <div className={classes.finder}>
-        <input type="search" onChange={searchChangeHandler} />
-      </div>
-      <div className={classes.finder}>
-        <Users users={filteredUsers} />
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <div className={classes.finder}>
+//         <input type="search" onChange={searchChangeHandler} />
+//       </div>
+//       <div className={classes.finder}>
+//         <Users users={filteredUsers} />
+//       </div>
+//     </div>
+//   );
+// };
+// }
 
 export default UserFinder;
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { Component, useState } from "react";
+import { Component } from "react";
 
 import User from "./User";
 import classes from "./Users.module.css";
