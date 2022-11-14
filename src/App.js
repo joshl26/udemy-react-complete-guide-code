077de128d2dd2d9623fa605f28531e5df74c8614
joshl26 <joshl26@hotmail.com>
2022-11-13 22:14:40 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -13,11 +13,11 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        "https://react-http-84069-default-rtdb.firebaseio.com/tasks.json"
       );
 
       if (!response.ok) {
-        throw new Error('Request failed!');
+        throw new Error("Request failed!");
       }
 
       const data = await response.json();
@@ -30,7 +30,7 @@ function App() {
 
       setTasks(loadedTasks);
     } catch (err) {
-      setError(err.message || 'Something went wrong!');
+      setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
   };
