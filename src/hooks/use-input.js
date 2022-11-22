27873e473e407a35222ef7b01fb3b9d6250afbc1
joshl26 <joshl26@hotmail.com>
2022-11-22 12:31:48 -0500

    commit

---
+++ src/hooks/use-input.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+  };
+
+  return {
+    value: enteredValue, hasError: hasError, 
+  }
+};
+
+export default useInput;
