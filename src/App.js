17f945b05e2caf1be50e3ab0804e7770fa917e4f
joshl26 <joshl26@hotmail.com>
2022-11-22 17:39:53 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import BasicForm from './components/BasicForm';
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div className="app">
-      <BasicForm />
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
