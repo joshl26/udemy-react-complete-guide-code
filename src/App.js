5747a6818158288893fc3c6201cb289447865dfd
joshl26 <joshl26@hotmail.com>
2022-11-22 10:11:09 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
-import React, { useEffect, useState } from "react";
-
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
-import useHTTP from "./components/hooks/use-http";
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const transformTasks = (tasksObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHTTP(
-    {
-      url: "https://react-http-99475-default-rtdb.firebaseio.com/tasks.json",
-    },
-    transformTasks
-  );
-
-  useEffect(() => {
-    fetchTasks();
-  }, [fetchTasks]);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
   return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
