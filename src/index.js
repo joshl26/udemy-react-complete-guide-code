772e8609ac2919adf375443fab20ce8350e37be0
joshl26 <joshl26@hotmail.com>
2022-11-23 10:55:34 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./store/index";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
+import store from './store/index';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App />
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    counter: state.counter + 1;
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
   }
-  if (action.type === "decrement") {
-    counter: state.counter - 1;
+
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+    };
   }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
   return state;
 };
 
