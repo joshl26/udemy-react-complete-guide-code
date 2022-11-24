ef744c2da09c6460367a215fe31ad8cd20b7844a
joshl26 <joshl26@hotmail.com>
2022-11-24 16:14:31 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-
 function App() {
   return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
