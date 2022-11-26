bee70f623bf73c930cc8214a6ddadc97868b33f8
joshl26 <joshl26@hotmail.com>
2022-11-26 11:39:06 -0500

    commit

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+const ProductDetail = () => {
+  const params = useParams();
+
+  return (
+    <section>
+      <h1>Product Detail</h1>
+      <p>{params.productId}</p>
+    </section>
+  );
+};
+
+export default ProductDetail;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-    return <h1>The Welcome page</h1>;
+  return (
+    <section>
+      <h1>The Welcome page</h1>
+      <Route path="/welcome/new-user">
+        <h1>Welcome, New User!</h1>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
