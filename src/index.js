646f17a353eda153ea090b33ea5fbf30cceb03c2
joshl26 <joshl26@hotmail.com>
2022-11-26 11:48:19 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
