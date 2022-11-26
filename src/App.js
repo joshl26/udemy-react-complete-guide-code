519803266258bfc72ae3507522d815cdd99ef14a
joshl26 <joshl26@hotmail.com>
2022-11-26 12:22:12 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
