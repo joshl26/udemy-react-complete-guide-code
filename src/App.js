6150ea1ab7f580cc05d1ec49faf7a72b2bb8c510
joshl26 <joshl26@hotmail.com>
2022-11-27 22:01:20 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
         </Route>
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
