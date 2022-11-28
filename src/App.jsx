0639f5c3dfd1a9cfea2718ad7e6fc850699df215
joshl26 <joshl26@hotmail.com>
2022-11-28 17:24:43 -0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import BlogLayout from "./pages/BlogLayout";
+import BlogPostsPage, { loader as blogPostsLoader } from "./pages/BlogPosts";
+import NewPostPage from "./pages/NewPost";
+import PostDetailPage from "./pages/PostDetail";
+import RootLayout from "./pages/RootLayout";
+import WelcomePage from "./pages/Welcome";
+import { loader } from "./pages/BlogPosts";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+        <Route path=":id" element={<PostDetailPage />} />
+      </Route>
+      <Route path="/blog/new" element={<NewPostPage />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/BlogPosts.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+import Posts from "../components/Posts";
+import { getPosts } from "../util/api";
+
+function BlogPostsPage() {
+  const loaderData = useLoaderData();
+
+  return (
+    <>
+      <h1>Our Blog Posts</h1>
+      <Posts blogPosts={loaderData} />
+    </>
+  );
+}
+
+export default BlogPostsPage;
+
+export function loader() {
+  return getPosts();
+}
