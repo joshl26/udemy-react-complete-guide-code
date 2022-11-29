f3591e186580b72028e8270f2f0eac5e07f8a504
joshl26 <joshl26@hotmail.com>
2022-11-29 13:53:56 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Greeting from "./components/Greeting";
+import Async from "./components/Async";
 
 function App() {
   return (
     <div className="App">
       <Greeting />
+      <Async />a
     </div>
   );
 }
