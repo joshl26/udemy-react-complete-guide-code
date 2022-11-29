0205fe08dcdc6daa1912cf404745e84dafebde55
joshl26 <joshl26@hotmail.com>
2022-11-29 09:38:47 -0500

    commit

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>Its good to see you!</p>
+      {!changedText && <p>Its good to see you!</p>}
+      {changedText && <p>Changed!</p>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders hello world as a text", () => {
-  //Arrange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders hello world as a text", () => {
+    //Arrange
+    render(<Greeting />);
 
-  //Act
-  //... nothing
+    //Act
+    //... nothing
 
-  //Assert
-  const helloWorldElement = screen.getByText("Hello World!");
-  expect(helloWorldElement).toBeInTheDocument();
+    //Assert
+    const helloWorldElement = screen.getByText("Hello World!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders its good to see as text", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    //... nothing
+
+    //Assert
+    const unchangedTextElement = screen.getByText("good to see you", {
+      exact: false,
+    });
+    expect(unchangedTextElement).toBeInTheDocument();
+  });
 });
