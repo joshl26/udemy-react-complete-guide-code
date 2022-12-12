8c12cb1c9a366b79cbb648865e2df91abd4b8432
joshl26 <joshl26@hotmail.com>
2022-12-12 10:37:28 -0500

    commit

---
+++ src/store/ui-slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSLice = createSlice({
+  name: "ui",
+  initialState: { cartIsVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
+
+export const uiActions = uiSLice.actions;
+
+export default uiSLice;
