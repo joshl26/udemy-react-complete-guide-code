c67255b8ee30ee45a1d0998060ab4292ff57a60c
joshl26 <joshlehman.dev@gmail.com>
2022-12-28 11:24:07 -0500

    commit

---
+++ basics.ts
@@ -0,0 +1,17 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+//Function types, parameters
+
+//Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
