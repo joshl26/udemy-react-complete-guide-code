8a92ae539533c1cf28964f678a52d2d3122a16e4
joshl26 <joshlehman.dev@gmail.com>
2022-12-28 23:28:27 -0500

    commit

--- basics.ts
+++ basics.ts
@@ -43,3 +43,21 @@ course = 12341;
 function add(a: number, b: number) {
   return a + b;
 }
+function print_output(value: any) {
+  console.log(value);
+}
+
+//Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1,1,2,3]
+
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+
+//updatedArray[0].split("");
