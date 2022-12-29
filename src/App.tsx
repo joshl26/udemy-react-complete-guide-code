e4de28c5b55783d31b77ce9b454e13d9d9c51860
joshl26 <joshlehman.dev@gmail.com>
2022-12-28 23:46:46 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import ToDos from "./components/ToDos";
 
 function App() {
   return (
     <div className="App">
+      <ToDos />
     </div>
   );
 }
---
+++ src/components/ToDos.tsx
@@ -0,0 +1,10 @@
+function ToDos() {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Learn TypeScript</li>
+    </ul>
+  );
+}
+
+export default ToDos;
